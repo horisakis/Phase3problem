ddaa10478a6db3ea10538f358d86d16a294d85e6
horisaki <seiji.horisaki@gmail.com>
2018-06-03 14:04:56 +0900

    バリデーション

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,4 +1,6 @@
 class Picture < ApplicationRecord
+  validates :image, presence: true
+  validates :user_id, presence: true
   belongs_to :user
   has_many :favorites, dependent: :destroy
   mount_uploader :image, ImageUploader
