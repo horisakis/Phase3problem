4ec55d13df447bfc668ccc5c2df7e2f76a0b3878
horisaki <seiji.horisaki@gmail.com>
2018-06-07 00:48:41 +0900

    トップページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 
+  get '/', to: 'static_pages#index'
+
   resources :sessions, only: %i[new create destroy]
 
   resources :users do
