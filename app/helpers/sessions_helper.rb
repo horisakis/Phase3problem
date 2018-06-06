17b219f92d632215d8b1a15df8f9ab9df6c859f7
horisaki <seiji.horisaki@gmail.com>
2018-06-06 21:22:40 +0900

    カレントユーザーとパラメータのIDが不一致の場合のガードを追加

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,10 +7,10 @@ module SessionsHelper
     current_user.present?
   end
 
-  def redirect_to_log_in
+  def redirect_to_log_in(user_id)
     if current_user.nil?
       redirect_to new_session_path
-    elsif current_user.id != params[:user_id]
+    elsif current_user.id != user_id.to_i
       redirect_to '/'
     end
   end
