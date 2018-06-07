4c6156eaf383b58497bbd1df6e41070408d64e11
horisaki <seiji.horisaki@gmail.com>
2018-06-08 00:49:06 +0900

    through追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,5 @@ class User < ApplicationRecord
 
   has_many :pictures, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :favorite_pictures, through: :favorites, source: :picture
 end
