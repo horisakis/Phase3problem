b7f6497b48671b116cac1b61bdf25c019040e025
horisaki <seiji.horisaki@gmail.com>
2018-06-08 01:47:00 +0900

    user一覧のルーティングを首絞め

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   resources :sessions, only: %i[new create destroy]
 
-  resources :users do
+  resources :users, except: :index do
     resources :pictures do
       collection do
         post :confirm
