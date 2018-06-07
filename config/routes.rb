a2eadb544b484e6b8559b7c325996dad238d5bc2
horisaki <seiji.horisaki@gmail.com>
2018-06-08 00:50:28 +0900

    root_pathに対応

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 
-  get '/', to: 'static_pages#index'
+  root 'static_pages#index'
 
   resources :sessions, only: %i[new create destroy]
 
