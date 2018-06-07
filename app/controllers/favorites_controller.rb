9ee07282365319ac89e86291e1e924f81351bff0
horisaki <seiji.horisaki@gmail.com>
2018-06-08 00:55:13 +0900

    favoriteのレイアウト修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,25 +1,26 @@
 class FavoritesController < ApplicationController
-  before_action :set_favorite, only: %i[show edit update destroy]
-  before_action -> {redirect_to_log_in(params[:user_id])}, except: :index
+  before_action :set_favorite, only: :destroy
+  before_action -> { redirect_to_log_in(params[:user_id]) }, except: :index
 
   # GET /favorites
   # GET /favorites.json
   def index
-    @favorites = current_user.favorites
+    @user = User.find(params[:user_id])
+    @favorites = @user.favorites
+    @favorite_pictures = @favorites.map(&:picture)
   end
 
   # POST /favorites
   # POST /favorites.json
   def create
     @favorite = current_user.favorites.new(picture_id: params[:picture_id])
-    @favorite.user_id = current_user.id
-
+    # @favorite.user_id = current_user.id
     respond_to do |format|
       if @favorite.save
-        format.html { redirect_to params[:back_path], notice: 'Favorite was successfully created.' }
+        format.html { redirect_back fallback_location: root_path, notice: 'Favorite was successfully created.' }
         format.json { render :show, status: :created, location: @favorite }
       else
-        format.html { render params[:back_path] }
+        format.html { redirect_back fallback_location: root_path }
         format.json { render json: @favorite.errors, status: :unprocessable_entity }
       end
     end
@@ -28,11 +29,9 @@ class FavoritesController < ApplicationController
   # DELETE /favorites/1
   # DELETE /favorites/1.json
   def destroy
-    @favorite = current_user.favorites.new(favorite_params)
-
     @favorite.destroy
     respond_to do |format|
-      format.html { redirect_to params[:back_path], notice: 'Favorite was successfully destroyed.' }
+      format.html { redirect_back fallback_location: root_path, notice: 'Favorite was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -46,6 +45,6 @@ class FavoritesController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def favorite_params
-    params.require(:favorite).permit(:picture_id)
+    params.require(:favorite).permit(:picture_id, :user_id)
   end
 end
